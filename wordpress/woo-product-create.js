77af4b549cada753fb71eb68a1198c4cd8f51fd0
Paul Sealock <psealock@gmail.com>
2017-09-13 16:15:31 -0700

    create product: handle error

--- wordpress/woo-product-create.js
+++ wordpress/woo-product-create.js
@@ -26,8 +26,8 @@ var data = {
 	]
 };
 
-module.exports = function(RED) {
-	const WooCommerceAPI = require('woocommerce-api');
+module.exports = function( RED ) {
+	const WooCommerceAPI = require( 'woocommerce-api' );
 	function wooProductCreate( config ) {
 		const node = this;
 		RED.nodes.createNode( node, config );
@@ -35,18 +35,23 @@ module.exports = function(RED) {
 		node.siteconfig = RED.nodes.getNode( node.config.config );
 
 		this.on( 'input', function( msg ) {
-			var WooCommerce = new WooCommerceAPI({
+			var WooCommerce = new WooCommerceAPI( {
 				url: node.siteconfig.url,
 				consumerKey: node.siteconfig.credentials.key,
 				consumerSecret: node.siteconfig.credentials.secret,
 				wpAPI: true,
 				version: 'wc/v1'
-			});
-			WooCommerce.post('products', data, function(err, data, res) {
-				console.log(res);
-			});
-			msg.payload = typeof WooCommerceAPI;
-			node.send( msg );
+			} );
+			WooCommerce.post( 'products', data, function( err, data, res ) {
+				if ( err ) {
+					console.log( err );
+					msg.payload = 'There was an error, check the console';
+				} else {
+					msg.payload = res;
+				}
+				node.send( msg );
+			} );
+
 		} );
 	};
 
