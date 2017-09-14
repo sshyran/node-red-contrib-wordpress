921ef7ea8a77c411d5e2d104adfbadbdd91f74b4
Paul Sealock <psealock@gmail.com>
2017-09-14 11:55:45 -0700

    Add Field labels to Woo nodes

--- wordpress/woo-product-create.js
+++ wordpress/woo-product-create.js
@@ -1,31 +1,4 @@
 "use strict";
-
-var data = {
-	name: 'Test 1',
-	type: 'simple',
-	regular_price: '21.99',
-	description: 'Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Vestibulum tortor quam, feugiat vitae, ultricies eget, tempor sit amet, ante. Donec eu libero sit amet quam egestas semper. Aenean ultricies mi vitae est. Mauris placerat eleifend leo.',
-	short_description: 'Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas.',
-	categories: [
-		{
-			id: 9
-		},
-		{
-			id: 14
-		}
-	],
-	images: [
-		{
-			src: 'http://demo.woothemes.com/woocommerce/wp-content/uploads/sites/56/2013/06/T_2_front.jpg',
-			position: 0
-		},
-		{
-			src: 'http://demo.woothemes.com/woocommerce/wp-content/uploads/sites/56/2013/06/T_2_back.jpg',
-			position: 1
-		}
-	]
-};
-
 module.exports = function( RED ) {
 	const WooCommerceAPI = require( 'woocommerce-api' );
 	function wooProductCreate( config ) {
@@ -42,7 +15,7 @@ module.exports = function( RED ) {
 				wpAPI: true,
 				version: 'wc/v1'
 			} );
-			WooCommerce.post( 'products', data, function( err, data, res ) {
+			WooCommerce.post( 'products', msg.payload, function( err, data, res ) {
 				if ( err ) {
 					console.log( err );
 					msg.payload = 'There was an error, check the console';
@@ -51,7 +24,6 @@ module.exports = function( RED ) {
 				}
 				node.send( msg );
 			} );
-
 		} );
 	};
 
